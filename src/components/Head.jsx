f47ac6436eec30361255b0abcb40da84f58dbb79
Ivan Falcon <Falconivanernesto@gmail.com>
2023-09-13 08:49:46 -0300

    First commit

---
+++ src/components/Head.jsx
@@ -0,0 +1,11 @@
+import "../assets/styles/Head.css";
+function Head() {
+    return (
+        <>
+            <header>
+                <h1>IT's Food</h1>
+            </header>
+        </>
+    );
+}
+export default Head;
---
+++ src/components/Primary.jsx
@@ -0,0 +1,22 @@
+import "../assets/styles/Primary.css";
+
+function Primary() {
+    return (
+        <>
+            <main>
+                <ul className="list-container">
+                    <li>
+                        <a href="">Menú</a>
+                    </li>
+                    <li>
+                        <a href="">Locales</a>
+                    </li>
+                    <li>
+                        <a href="">Contacto</a>
+                    </li>
+                </ul>
+            </main>
+        </>
+    );
+}
+export default Primary;
