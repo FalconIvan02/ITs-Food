e18052a42f5c4db84f43d561b15975b9417f755f
Ivan Falcon <Falconivanernesto@gmail.com>
2023-09-13 08:07:12 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Primary from "./components/Primary";
+import Footer from "./components/Footer";
+import Head from "./components/Head";
+
+function App() {
+    return (
+        <>
+            <Head />
+            <Primary />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
